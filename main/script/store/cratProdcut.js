79a683cd5b528902330dc1f52961019404558753
mahdi <m.rezaei.coder@gmail.com>
2025-03-08 17:08:44 +0330

    Fixing the bug of getting products from local storage

--- main/script/store/cratProdcut.js
+++ main/script/store/cratProdcut.js
@@ -1,11 +1,12 @@
-let productsToCart = []
 export let productCart = (product) => {
     // گرفتن آرایه محصولات سبد خرید و جلوگیری از پاک شدن این اصلاعات
-    let getCartProducts = JSON.parse(localStorage.getItem("cartProducts")) || []
+    let getCartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
+    let comvertOBJdataToArry = Object.values(getCartProducts)
+    
     // اضافه کردن اطلاعات محصول جدید به آرایه
-    productsToCart.push(product);
+    comvertOBJdataToArry.push(product);
     // بروز رسانی آرایه با محصول جدید و ذخیره کردن در لوکال استوریج
-    localStorage.setItem("cartProducts", JSON.stringify(productsToCart))
+    localStorage.setItem("cartProducts", JSON.stringify(comvertOBJdataToArry));
 }
 
 export default productCart;
