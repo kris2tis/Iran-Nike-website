a9052f222fae3183e77b3b1de85c60f1beebc1a5
mahdi <m.rezaei.coder@gmail.com>
2025-03-09 20:59:53 +0330

    سیب

--- script/form/doesUsserLogged.js
+++ script/form/doesUsserLogged.js
@@ -33,10 +33,11 @@ export function usserLogged() {
             document.querySelector(".quit-account").style.display = "block";
             document.querySelector(".btns").style.gap = "0px";
         } else {
+            const baseUrl = window.location.origin + "/Iran-Nike-website/";
             dynamicHeader.insertAdjacentHTML(
                 "beforeend",
                 `<div class="signin-signup basic-gray">
-                           <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? './login.html' : './html/logIn.html'}" class="basic-flex">
+                          <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? baseUrl + 'login.html' : baseUrl + 'html/logIn.html'}" class="basic-flex">
                             <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"></path>
                             </svg>
                         </a>
