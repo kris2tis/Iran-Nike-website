43c836084db2f90413ae31374f1908d67f8b3321
mahdi <m.rezaei.coder@gmail.com>
2025-03-09 20:26:51 +0330

    dncm addres - 8

--- script/form/doesUsserLogged.js
+++ script/form/doesUsserLogged.js
@@ -36,7 +36,7 @@ export function usserLogged() {
             dynamicHeader.insertAdjacentHTML(
                 "beforeend",
                 `<div class="signin-signup basic-gray">
-                        <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? `../html/login.html` : `./html/logIn.html` }" class="basic-flex">
+                        <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? `/login.html` : `/html/logIn.html` }" class="basic-flex">
                             <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"></path>
                             </svg>
                         </a>
