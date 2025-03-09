9ae6b5950d8c8b9d6532873ee66c4ec55270a4e8
mahdi <m.rezaei.coder@gmail.com>
2025-03-09 20:44:31 +0330

    daynamic header-10

--- script/form/doesUsserLogged.js
+++ script/form/doesUsserLogged.js
@@ -13,7 +13,7 @@ export function usserLogged() {
             dynamicHeader.insertAdjacentHTML(
                 "beforeend",
                 `<div class="cart basic-gray">
-                <a href="${window.location.pathname == `/Iran-Nike-website/html/store.html` ? `../cart.html` : `./html/cart.html` }" class="basic-flex">
+                <a href="${window.location.pathname == `/Iran-Nike-website/html/store.html` ? `./cart.html` : `./html/cart.html` }" class="basic-flex">
                 <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path></svg>
                 </a>
                 <div class="count-of-prodict-cart basic-flex">${cartProduct.length}</div>
@@ -36,7 +36,7 @@ export function usserLogged() {
             dynamicHeader.insertAdjacentHTML(
                 "beforeend",
                 `<div class="signin-signup basic-gray">
-                           <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? '../login.html' : './html/logIn.html'}" class="basic-flex">
+                           <a href="${window.location.pathname == '/Iran-Nike-website/html/store.html' ? './login.html' : './html/logIn.html'}" class="basic-flex">
                             <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"></path>
                             </svg>
                         </a>
